504ec8277f1d416cd5e448faaef30fcc96c6b419
Bob Carter <46971019+bob-carter@users.noreply.github.com>
2026-05-02 20:36:10 +0000

    Just a change of port
    Title

--- tools/build/build.ts
+++ tools/build/build.ts
@@ -414,7 +414,7 @@ export const ServerTarget = new Juke.Target({
   parameters: [DmVersionParameter, PortParameter],
   dependsOn: [BuildTarget],
   executes: async ({ get }) => {
-    const port = get(PortParameter) || '1337';
+    const port = get(PortParameter) || '3000';
     const options = {
       dmbFile: `${DME_NAME}.dmb`,
       namedDmVersion: get(DmVersionParameter),
